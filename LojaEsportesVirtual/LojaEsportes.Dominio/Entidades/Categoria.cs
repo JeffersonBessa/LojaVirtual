d881f3cb48b8d168843f0646e60199d8f661ea23
jefferson <jefferson.bessa10@gmail.com>
2017-05-19 19:56:17 -0300

    Versão 1.1

---
+++ LojaEsportesVirtual/LojaEsportes.Dominio/Entidades/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LojaEsportes.Dominio.Entidades
+{
+    [Table("Categorias")]
+    public class Categoria
+    {
+        [Display(Name = "Categoria")]
+        public int CategoriaId { get; set; }
+        [Required(ErrorMessage = "Informe o nome da catagoria")]
+        public string Nome { get; set; }
+        public virtual ICollection<Produto> Produtos { get; set; }
+    }
+}
---
+++ LojaEsportesVirtual/LojaEsportes.Dominio/Repositorio/ProdutoRepositorio.cs
@@ -0,0 +1,28 @@
+﻿using LojaEsportes.Dominio.Entidades;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LojaEsportes.Dominio.Repositorio
+{
+    public class ProdutoRepositorio : IRepositorio<Produto>
+    {
+        private ProdutoContexto contexto;
+
+        public ProdutoRepositorio(ProdutoContexto produtoContexto)
+        {
+            this.contexto = produtoContexto;
+        }
+
+        //todos os produtos
+        public IEnumerable<Produto> GetTodos()
+        {
+            return contexto.Produtos.ToList().OrderBy(p=> p.Nome);
+        }
+
+        //produtos por categoria
+        public IEnumerable<Produto> Get(int? id)
+        {
+            return contexto.Produtos.Where(c => c.CategoriaId == id).OrderBy(p => p.Nome);
+        }
+    }
+}
